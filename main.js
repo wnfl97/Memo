08fe2e8d0e0df205b7c93f8ddce470b5558688ef
wnfl97 <85559294+wnfl97@users.noreply.github.com>
2021-09-08 17:34:45 +0900

    update js with delegation

--- main.js
+++ main.js
@@ -12,37 +12,50 @@ function onAdd(){
     }
     const item = createItem(text);
     items.appendChild(item);
+    item.scrollIntoView({block:'center'});
     input.value='';
     input.focus();
 }
-
+let id = 0;
 function createItem(text){
     const itemRow = document.createElement('li');
     itemRow.setAttribute('class','item__row');
+    itemRow.setAttribute('data-id',id); // 구분선 포함한 한 줄을 지워야해서
+    itemRow.innerHTML=`
+        <div class="item" >
+            <span class="item__name">${text}</span>
+            <button class="item__del" >
+                <i class="fas fa-trash" data-id=${id}></i>
+            </button>
+        </div>
+        <div class="divider"></div>
+    `;
+    id++;
 
-    const item = document.createElement('div');
-    item.setAttribute('class','item');
+    // const item = document.createElement('div');
+    // item.setAttribute('class','item');
 
-    const span = document.createElement('span');
-    span.setAttribute('class','item__name');
-    span.innerText= text;
+    // const span = document.createElement('span');
+    // span.setAttribute('class','item__name');
+    // span.innerText= text;
 
-    const divider = document.createElement('div');
-    divider.setAttribute('class','divider');
+    // const divider = document.createElement('div');
+    // divider.setAttribute('class','divider');
 
-    const delBtn = document.createElement('button');
-    delBtn.setAttribute('class','item__del');
-    delBtn.innerHTML=`<i class="fas fa-trash"></i>`;
+    // const delBtn = document.createElement('button');
+    // delBtn.setAttribute('class','item__del');
+    // delBtn.innerHTML=`<i class="fas fa-trash"></i>`;
 
-    delBtn.addEventListener('click',()=>{
-        items.removeChild(itemRow);
-    });
+    // delBtn.addEventListener('click',()=>{
+    //     items.removeChild(itemRow);
+    // });
 
-    item.appendChild(span);
-    item.appendChild(delBtn);
+    // item.appendChild(span);
+    // item.appendChild(delBtn);
 
-    itemRow.appendChild(item);
-    itemRow.appendChild(divider);
+    // itemRow.appendChild(item);
+    // itemRow.appendChild(divider);
+
     return itemRow;
 }
 
@@ -54,4 +67,16 @@ input.addEventListener('keypress', event=>{
     if(event.keyCode === 13){
         return onAdd();
     }
+});
+
+items.addEventListener('click', event=>{
+    const id = event.target.dataset.id;
+    if(id){
+    // event.target.nodeName === 'I'보다 낫다. 
+    // source에서 dataset.id:'0' 출력
+    const toBeDelete = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDelete.remove();
+    };
+    
+
 });
